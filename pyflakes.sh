a047154302e1ab3a84e3392325f8ecce7eda4d0e
Christopher Trudeau <ctrudeau@arsensa.com>
2025-10-13 11:04:16 -0400

    Since purdy 2.0 is a complete re-write, this commit wipes everything to make the merge simpler

--- pyflakes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "============================================================"
-echo "== pyflakes =="
-pyflakes purdy tests extras/samples extras/tools
-
-echo "============================================================"
-echo "== loggers =="
-pygrep "logger" | grep -v "#" | grep -v "extras/tools"
